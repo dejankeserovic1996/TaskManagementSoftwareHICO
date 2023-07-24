84b4cb35c1e23edcc3a61ad7d0b13330eddf28d8
Dejan <dejan.keserovic@outlook.com>
2023-07-24 07:11:43 +0200

    Added task controller

--- TestWebAPI/Controllers/TaskController.cs
+++ TestWebAPI/Controllers/TaskController.cs
@@ -8,6 +8,165 @@ namespace HICOWebAPI.Controllers
 {
     public class TasksController : ControllerBase
     {
+        [HttpPost("api/tasks")]
+        public IActionResult CreateTask(Task task)
+        {
+            task.ID = Guid.NewGuid();
+            TaskService.tasks.Add(task);
+            return Ok(task);
+        }
 
+        [HttpGet("api/tasks/{id}")]
+        public IActionResult GetTask(Guid id)
+        {
+            var task = TaskService.tasks.FirstOrDefault(m => m.ID == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        [HttpGet("api/tasks/material/{id}")]
+        public IActionResult GetMaterial(Guid id)
+        {
+            var task = TaskService.tasks.FirstOrDefault(m => m.ID == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task.TaskMaterialUsage.Material);
+        }
+
+        [HttpGet("api/tasks")]
+        public IEnumerable<Task> GetTasks()
+        {
+            return TaskService.tasks;
+        }
+
+        [HttpPost]
+        [Route("api/create/task/{materialId}")]
+        public IActionResult CreateATaskWithMaterial(Guid materialId, [FromBody] Task task)
+        {
+            task.ID = Guid.NewGuid();
+            TaskService.tasks.Add(task);
+
+            var material = MaterialService.materials.FirstOrDefault(m => m.ID == materialId);
+
+            if (material == null)
+            {
+                return NotFound();
+            }
+
+            if (!UnitService.CanSwitchUnit(task.TaskMaterialUsage.UnitOfMeasurement, material.UnitOfIssue))
+            {
+                return BadRequest("Invalid unit switch.");
+            }
+
+            task.TaskMaterialUsage.Task = task;
+            task.TaskMaterialUsage.Material = material;
+            material.TaskMaterialUsages.Add(task.TaskMaterialUsage);
+
+            return Ok(task);
+        }
+
+        [HttpPut]
+        [Route("api/tasks/{materialId}")]
+        public IActionResult UpdateTask(Guid materialId, [FromBody] Task task)
+        {
+            var material = MaterialService.materials.FirstOrDefault(m => m.ID == materialId);
+            var updatedTask = TaskService.tasks.FirstOrDefault(m => m.ID == task.ID);
+
+            if (material == null || updatedTask == null)
+            {
+                return NotFound();
+            }
+
+            if (!UnitService.CanSwitchUnit(task.TaskMaterialUsage.UnitOfMeasurement, material.UnitOfIssue))
+            {
+                return BadRequest("Invalid unit switch.");
+            }
+
+            updatedTask.TaskMaterialUsage.Material.TaskMaterialUsages.RemoveAll(o => o.Task == updatedTask);
+            updatedTask.TaskMaterialUsage.Material.TaskMaterialUsages.Add(updatedTask.TaskMaterialUsage);
+            updatedTask.TaskMaterialUsage.Material = material;
+            updatedTask.TaskMaterialUsage.Task = updatedTask;
+            updatedTask.TaskMaterialUsage.UnitOfMeasurement = task.TaskMaterialUsage.UnitOfMeasurement;
+            updatedTask.TaskMaterialUsage.Amount = task.TaskMaterialUsage.Amount;
+            updatedTask.TotalDuration = task.TotalDuration;
+            updatedTask.Description = task.Description;
+            updatedTask.ID = task.ID;
+            updatedTask.Name = task.Name;
+
+            return Ok(task);
+        }
+
+        [HttpGet("api/units")]
+        public IActionResult GetUnits()
+        {
+            var enumValues = Enum.GetValues(typeof(Unit));
+            return Ok(enumValues);
+        }
+
+        [HttpGet("api/units/{materialId}")]
+        public IActionResult GetUnits(Guid materialId)
+        {
+            var material = MaterialService.materials.FirstOrDefault(m => m.ID == materialId);
+
+            if (material == null)
+            {
+                return NotFound();
+            }
+
+            var units = UnitService.unitList.FirstOrDefault(list => list.Contains(material.UnitOfIssue));
+
+            if (units == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(units);
+        }
+
+        [HttpPost]
+        [Route("api/tasks/{taskId}/addmaterial/{materialId}")]
+        public IActionResult AddMaterialToTask(Guid taskId, Guid materialId, TaskMaterialUsage usage)
+        {
+            var task = TaskService.tasks.FirstOrDefault(t => t.ID == taskId);
+            var material = MaterialService.materials.FirstOrDefault(m => m.ID == materialId);
+
+            if (task == null || material == null)
+            {
+                return NotFound();
+            }
+
+            if (!UnitService.CanSwitchUnit(usage.UnitOfMeasurement, material.UnitOfIssue))
+            {
+                return BadRequest("Invalid unit switch.");
+            }
+
+            usage.Task = task;
+            usage.Material = material;
+            task.TaskMaterialUsage = usage;
+
+            return Ok(task);
+        }
+
+        [HttpDelete("api/tasks/{id}")]
+        public IActionResult DeleteTask(Guid id)
+        {
+            var task = TaskService.tasks.FirstOrDefault(m => m.ID == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            task.TaskMaterialUsage.Material.TaskMaterialUsages.RemoveAll(o => o.Task == task);
+            TaskService.tasks.Remove(task);
+
+            return Ok();
+        }
     }
 }
